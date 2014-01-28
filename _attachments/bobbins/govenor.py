9289acb5f41736ac024afde114856eb9678a13d5
root <root@debian>
2014-01-28 08:48:46 -0500

    auto spindle starter , needs work

--- _attachments/bobbins/govenor.py
+++ _attachments/bobbins/govenor.py
@@ -7,12 +7,17 @@ class govenor(adapter.worker):
 		self.bobbins = []
 	
 	def consume(self,body):
-		print 'govenor',body
+		print 'govenor gets ',body
 		if 'status' in body:
 			print 'status'
-#			get_queue_stat(self.cq)
+			get_queue_stat(self.cq)
 		if 'info' in body:
 			print self.bobbins
+		if 'spindle' in body:
+			d = get_queue_stat(self.cq)
+			for j in d.keys():
+				print 'start '+j
+				self.cq.channel.basic_publish('',body['spindle']['queue'],adapter.encode({'start_bobbin':j}))
 		if 'bobbin' in body:
 			self.bobbins.append(body['bobbin'])
 		if 'start_bobbin' in body:
@@ -26,16 +31,15 @@ def get_queue_stat(cq):
     cred = cq.local_config['broker_cred']
     s.auth = (cred[0],cred[1])
     broker = cq.config['broker'][0]
-    r = s.get('http://'+broker+':15672/api/queues')
+    r = s.get('http://'+broker+':55672/api/queues')
     queues = r.json()
-    bobbin_list = []
+    bobbin_list = {} 
     for i in queues:
         message_count = i['messages_ready']
         if message_count > 0:
-            print i['name'],i['messages_ready'],i['active_consumers']
             name = i['name']
             " don't respool yourself "
             if name != 'govenor' and name[0:3] !='amq':
-                bobbin_list.append(name)
+                bobbin_list[name] = message_count
     return bobbin_list
 
